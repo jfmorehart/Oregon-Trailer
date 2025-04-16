99a0d8c4d1a90b40dd19dc0cbcee1e4982d2335b
moonman13 <jfm9413@nyu.edu>
2025-04-16 15:02:17 -0400

    Gap Fixes

--- Assets/Scripts/LevelDrive/LevelCamera.cs
+++ Assets/Scripts/LevelDrive/LevelCamera.cs
@@ -20,7 +20,7 @@ public class LevelCamera : MonoBehaviour
 	void FixedUpdate()
     {
 		if (PlayerVan.vanTransform == null) return;
-		transform.position = PlayerVan.vanTransform.position - Vector3.forward;
+		transform.position = PlayerVan.vanTransform.position - Vector3.forward * 10;
 		Vector3 pos = transform.position - (Vector3)shake;
 
 		shake = shakestr * new Vector3(shakeAmp * (Mathf.PerlinNoise1D(Time.time * shakeFreq) - 0.5f), shakeAmp * (Mathf.PerlinNoise1D(Time.time * shakeFreq + 1) - 0.5f));
