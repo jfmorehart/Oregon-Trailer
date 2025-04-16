9b1ed207270186a2b96a265f8a18231d88c57628
DESKTOP-JEIT5SO\Sherry's office <aaroncjgill@gmail.com>
2025-04-16 01:03:02 -0400

    fixed visual bug with garage scene added new ending for nodes, along with choice In the nodes, you have three new parameters. twoStarTime and threeStarTime determines the time for their respective stars. The last is goToGarageScreenOnComplete which just opens the garage screen instead of the map screen when the level complete screen is finished.
    Stars act as money now as well.
    
    **The only thing to do is to make sure that the time is set in each level, Hunt selected as the activity and give each node an appropriate time. For nodes with special events or timed events just select None as the activity.**

--- Assets/Scripts/Minigames/Hunting/HuntManager.cs
+++ Assets/Scripts/Minigames/Hunting/HuntManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HuntManager : MonoBehaviour
@@ -7,6 +9,16 @@ public class HuntManager : MonoBehaviour
     [SerializeField]
     private GameObject huntScene;
     public static HuntManager instance;
+
+    public TMP_Text threeStarTimeText;
+    public TMP_Text twoStarTimeText;
+
+    public TMP_Text timeEarnedText;
+    public GameObject oneStar, twoStar, ThreeStar;
+
+    bool goToGarageScreenOnComplete = false;
+    int starsFromLevel = 0;
+
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -21,18 +33,42 @@ public class HuntManager : MonoBehaviour
     }
 
 
-    public void displayHunt()
+    public void displayHunt(bool goToGarageScreenOnComplete, float timeEarned, float twoStarTime, float threeStarTime, int starsEarned)
     {
 
-        //huntScene.SetActive(true);
-        hideHunt();
+        huntScene.SetActive(true);
+        //one star is active automatically
+        oneStar.SetActive(true);
+        if (starsEarned == 2)
+            twoStar.SetActive(true);
+        if (starsEarned == 3)
+            ThreeStar.SetActive(true);
+        this.goToGarageScreenOnComplete = goToGarageScreenOnComplete;
+        starsFromLevel = starsEarned;
+        timeEarnedText.text = (Math.Floor(twoStarTime/ 60) % 60).ToString("00") + ":" + Convert.ToInt32(twoStarTime % 60).ToString("00"); //TimeSpan.FromSeconds(timeEarned).ToString("hh':'mm':'ss");//string.Format("{0:t}", TimeSpan.FromSeconds(timeEarned));
+
+        twoStarTimeText.text = //string.Format("Two Star Time: {0:t}", twoStarTime );
+        threeStarTimeText.text = (Math.Floor(threeStarTime / 60) % 60).ToString("00") + ":" + Convert.ToInt32(threeStarTime % 60).ToString("00");//string.Format("Three Star Time: {0:t}", threeStarTime);
+
+
+        //hideHunt();
     }
     public void hideHunt()
     {
         huntScene.SetActive(false);
 
+
+        oneStar.SetActive(false);
+        twoStar.SetActive(false);
+        ThreeStar.SetActive(false);
+
+
+        timeEarnedText.text = "";
+        twoStarTimeText.text = "";
+        threeStarTimeText.text = "";
+
         //communicate with map manager to display the map
-        MapManager.instance.nodeActivityDone();
+        MapManager.instance.nodeActivityDone(goToGarageScreenOnComplete, starsFromLevel);
 
     }
 
